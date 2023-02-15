0d020a743f25394c46c5e8d3a81f6f6f761b33ff
bcsf19a019 <bcsf19a019@pucit.edu.pk>
2023-02-15 08:45:03 +0500

    Making IndexActivity

---
+++ app/src/main/java/com/example/quranapp/IndexActivity.java
@@ -0,0 +1,64 @@
+package com.example.quranapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndexActivity extends AppCompatActivity {
+
+    List<String> parahList = new ArrayList<>();
+    List<String> surrahList = new ArrayList<>();
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+
+        Intent intent = getIntent();
+        String searchCategory = intent.getStringExtra("category");
+
+        initializeDataInList();
+        recyclerView = findViewById(R.id.indexView);
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        if(searchCategory.equals("p"))
+        {
+            adapter = new IndexAdapter(getApplicationContext(), parahList) ;
+            recyclerView.setAdapter(adapter);
+        }
+        else if(searchCategory.equals("s"))
+        {
+            adapter = new IndexAdapter(getApplicationContext(), surrahList) ;
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+    private void initializeDataInList() {
+        for(int i=1; i<=30; i++)
+        {
+            String st = "Para ";
+            st = st+i;
+            parahList.add(st);
+        }
+
+        for(int i=1; i<=114; i++)
+        {
+            String st = "Sura ";
+            st = st+i;
+            surrahList.add(st);
+        }
+    }
+}
