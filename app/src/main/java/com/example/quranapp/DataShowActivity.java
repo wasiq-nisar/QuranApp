160a6df4ce95f87aa16bb12dd8beed9656a4f666
bcsf19a019 <bcsf19a019@pucit.edu.pk>
2023-02-15 08:58:18 +0500

    Making a new Activity

---
+++ app/src/main/java/com/example/quranapp/DataShowActivity.java
@@ -0,0 +1,14 @@
+package com.example.quranapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_show);
+    }
+}
