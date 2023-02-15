930d9be3f26e7913e4eac65768790e9e3dbbd356
bcsf19a019 <bcsf19a019@pucit.edu.pk>
2023-02-15 08:37:20 +0500

    Making Changes in MainActivity

--- app/src/main/java/com/example/quranapp/MainActivity.java
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -5,24 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    TextView helloText;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView parah,surrah;
+    String browseCategory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DBHandler databaseHelper = new DBHandler(this);
-        databaseHelper.createDataBase();
-        databaseHelper.openDataBase();
-        Cursor cursor = databaseHelper.query("SELECT * FROM QuranMetaData");
-        //helloText =  findViewById(R.id.helloText);
-        if(cursor.moveToFirst())
+
+        parah = findViewById(R.id.byParrah);
+        surrah = findViewById(R.id.bySurrah);
+
+        parah.setOnClickListener(this);
+        surrah.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
         {
-            helloText.setText(cursor.getString(1));
+            case R.id.byParrah:
+                browseCategory = "p";
+                loadIndex();
+                break;
+            case R.id.bySurrah:
+                browseCategory = "s";
+                loadIndex();
+                break;
         }
     }
 
+    private void loadIndex() {
+        Intent intent = new Intent(getApplicationContext(), IndexActivity.class);
+        if(browseCategory.equals("p"))
+        {
+            intent.putExtra("category","p");
+        }
+        else
+        {
+            intent.putExtra("category","s");
+        }
+        startActivity(intent);
+    }
 }
